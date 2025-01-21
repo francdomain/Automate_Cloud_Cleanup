10d59264d9f798059dc6e5c7cfe6e8fece784db8
francdomain <cfrancmail@gmail.com>
2025-01-21 05:39:13 +0100

    updated the pipeline

--- cloud_resource/github_role.tf
+++ cloud_resource/github_role.tf
@@ -6,6 +6,28 @@ resource "aws_iam_openid_connect_provider" "github" {
 }
 
 # 2. Create IAM Role
+# resource "aws_iam_role" "github_actions_role" {
+#   name = "github-actions-cleanup-role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Federated = aws_iam_openid_connect_provider.github.arn
+#         },
+#         Action = "sts:AssumeRoleWithWebIdentity",
+#         Condition = {
+#           StringEquals = {
+#             "token.actions.githubusercontent.com:sub" : "francdomain/Automate_Cloud_Cleanup:ref:refs/heads/main"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-cleanup-role"
 
@@ -20,7 +42,8 @@ resource "aws_iam_role" "github_actions_role" {
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:sub" : "francdomain/Automate_Cloud_Cleanup:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" : "repo:Automate_Cloud_Cleanup:ref:refs/heads/main"
           }
         }
       }
